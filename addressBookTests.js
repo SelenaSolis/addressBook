fd4552cd5888a66d717790a8e3827b36ab41cf29
Selena Solis <lenansolis@gmail.com>
2019-05-28 00:25:53 -0500

    more tests

--- addressBookTests.js
+++ addressBookTests.js
@@ -1,10 +1,12 @@
 "use strict;"
 
 const assert = require("assert");
+require('dotenv').config()
 const baseURL = "https://randomuser.me/api/?results=";
+const API_KEY = process.env.SECRET_KEY;
 
-function get(fetch, id){
-    return fetch("baseURL" + id)
+function get(fetch, id, key){
+    return fetch("baseURL" + id + "&key=" + key)
       .then( response => response.json())
       .then( data => data.results[0])
       .catch(err => console.error("err"))
@@ -18,7 +20,7 @@ describe("get()", function () {
             return new Promise(function(resolve){
             })
         }
-        get(fakeFetch, "15")
+        get(fakeFetch, "15", API_KEY)
     });
     it ('has correct base URL', function(){
         assert.deepEqual(baseURL, "https://randomuser.me/api/?results=")
@@ -28,5 +30,12 @@ describe("get()", function () {
             assert("https://randomuser.me/api/results=");
             assert.equal(get(errFetch), "err")
         }
+    })
+    it('has correct key', () => {
+        const fakeFetch = url =>{
+            assert(baseURL);
+            assert.equal(get(fakeFetch, "15", "lghejfslgde12k4n"), "err");
+        }
+
     })
 });
